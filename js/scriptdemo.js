ec9ee5af351b3dce614ca22761115d9dc2da67a8
Brandon W <bwirz75@gmail.com>
2017-10-11 13:25:32 -0600

    Update scriptdemo.js

--- js/scriptdemo.js
+++ js/scriptdemo.js
@@ -1,11 +1,10 @@
 var colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"];
-//var displayColors = [];
 
 var myText = "";
 for (var i = 0; i < colors.length; i++){
     myText += colors[i].toString() + " ";
 }
-//join(", ")44444444444444
+
 document.getElementById("colorNames").innerHTML = myText;
 document.getElementById("colorNames").style.fontWeight = 'bold';
 
